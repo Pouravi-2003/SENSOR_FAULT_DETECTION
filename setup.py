93243d99047c64542d7980448a90ccf7388d1550
Pouravi-2003 <pouravighosh@gmail.com>
2023-08-31 17:16:24 +0530

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="SENSOR-FAULT-DETECTION",
+    version="0.0.1",
+    author="Pouravi",
+    author_email="pouravighosh@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ upload_data.py
@@ -0,0 +1,11 @@
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://pwskills:<password>@cluster0.qxefg4l.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
